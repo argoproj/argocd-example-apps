b9df997b39afaac238b620bfc84d197200ba2b53
Christopher Quiles <chris.quiles@evercast.us>
2021-12-21 14:17:45 -0800

    chore: correcting bash script

--- scripts/test-appset.sh
+++ scripts/test-appset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 argocd app delete applicationset --cascade=false --grpc-web
-kubectl delete applicationsets test-appset --cascade=orphan -n argocd
-argocd app delete demo-test1 --cascade=false --grpc-web
-argocd app delete demo-test2 --cascade=false --grpc-web
+kubectl delete applicationset demo-apptest --cascade=orphan -n argocd
+argocd app delete test-test1 --cascade=false --grpc-web
+argocd app delete test-test2 --cascade=false --grpc-web
